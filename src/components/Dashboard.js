c4e7326ee15a0098ebc304b1964b677aa278f215
Micheala Palmer <michealapalmer@gmail.com>
2022-01-27 21:21:50 -0500

    handle states using this.setState and connect panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,17 +29,31 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
+  // instance method - that can take an id and set the state of focused to the value of id. Set the value of focused back to null if the value of focused is currently set to a panel.
+  selectPanel(id) {
+    this.setState(prevState => ({
+      focused: prevState.focused !== null ? null : id
+    }))
+  }
   render() {
-    const dashboardClasses = classnames("dashboard");
-    const panels = data.map((panel)=> {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
+    const panels = data
+    .filter(
+      panel => this.state.focused === null || this.state.focused === panel.id
+     )
+    .map((panel)=> {
       return (
         <Panel
           key={panel.id}
           id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)} // use this.selectPanel because we are passing a reference to the instance method as a prop.
         />
       );
     })
